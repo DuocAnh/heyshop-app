f9b13c99b8de0688e032362f851968dff1fca6be
DuocAnh <duocanh14@gmail.com>
2024-11-22 21:45:04 +0700

    handle search, filter, pagination products

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,17 +1,33 @@
 import Product from "../models/product.js";
+import APIFilters from "../utils/apiFilters.js";
+import ErrorHandler from "../utils/errorHandler.js";
 
 export const getProducts = async (req, res) => {
-  const products = await Product.find();
+  const resPerPage = 8;
+
+  const apiFilters = new APIFilters(Product, req.query).search().filters();
+
+  let products = await apiFilters.query;
+  let filteredProductCount = products.length;
+
+  apiFilters.pagination(resPerPage);
+  products = await apiFilters.query.clone();
 
   res.status(200).json({
+    resPerPage,
+    filteredProductCount,
     products,
   });
 };
 
-export const newProduct = async (req, res) => {
+export const newProduct = async (req, res, next) => {
   req.body.user = req.user._id;
 
   const product = await Product.create(req.body);
+
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
 
   res.status(200).json({
     product,
